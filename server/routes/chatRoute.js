8fdcc3968e8bf5c9b6606a698217583832a422f8
Aaryan Baral <aryanbaral1100@gmail.com>
2024-05-23 22:13:48 +0545

    get chat details and rename chat basic working api created

--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -2,12 +2,17 @@ import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import {
   addMembers,
+  deleteChatDetails,
+  getChatDetails,
   getMychats,
   getMyGroups,
   leaveGroup,
   newGroupChat,
   removeMember,
+  renameGroup,
+  sendAttachments,
 } from "../controllers/chatController.js";
+import { acttachmentsMulter } from "../middlewares/multer.js";
 const route = express.Router();
 
 route.use(isAuthenticated); // authentication is made compulsory to access any of the folowing routes
@@ -20,8 +25,10 @@ route.delete("/leave/:id",leaveGroup)
 
 // Send Attachments
 
+route.post("/message",acttachmentsMulter,sendAttachments)
 // Send Messages
 
+route.route("/:id").get(getChatDetails).put(renameGroup).delete(deleteChatDetails)
 // Get chat details,rename,delete
 
 export default route;
