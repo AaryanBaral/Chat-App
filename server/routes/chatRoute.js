c345f864f5f29962aa402366e2bfd7646692988e
Aaryan Baral <aryanbaral1100@gmail.com>
2024-05-24 15:58:58 +0545

    Validator added in routes

--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -14,23 +14,48 @@ import {
   sendAttachments,
 } from "../controllers/chatController.js";
 import { acttachmentsMulter } from "../middlewares/multer.js";
+import {
+  addMemberValidator,
+  chatIdValidator,
+  leaveGroupValidator,
+  newGroupValidator,
+  removeMemberValidator,
+  renameGroupValidator,
+  sendAttachmentsValidator,
+  validatorHandler,
+} from "../lib/validator.js";
 const route = express.Router();
 
 route.use(isAuthenticated); // authentication is made compulsory to access any of the folowing routes
-route.post("/new", newGroupChat);
+route.post("/new", newGroupValidator(), validatorHandler, newGroupChat);
 route.get("/my", getMychats);
 route.get("/my/groups", getMyGroups);
-route.put("/addmembers", addMembers);
-route.put("/removemember", removeMember);
-route.delete("/leave/:id",leaveGroup)
+route.put("/addmembers", addMemberValidator(), validatorHandler, addMembers);
+route.put(
+  "/removemember",
+  removeMemberValidator(),
+  validatorHandler,
+  removeMember
+);
+route.delete("/leave/:id", leaveGroupValidator(), validatorHandler, leaveGroup);
 
 // Send Attachments
-route.post("/message",acttachmentsMulter,sendAttachments)
+route.post(
+  "/message",
+  acttachmentsMulter,
+  sendAttachmentsValidator(),
+  validatorHandler,
+  sendAttachments
+);
 
 // Send Messages
-route.get("/message/:id",getMessages)
+route.get("/message/:id", chatIdValidator(), validatorHandler, getMessages);
 
 // Get chat details,rename,delete
-route.route("/:id").get(getChatDetails).put(renameGroup).delete(deleteChat)
+route
+  .route("/:id")
+  .get(chatIdValidator(), validatorHandler, getChatDetails)
+  .put(renameGroupValidator(), validatorHandler,renameGroup)
+  .delete(chatIdValidator(), validatorHandler, deleteChat);
 
 export default route;
